56ce7089ad140059ab7fc2f1b1305d405c4a6b37
Oleh <oleh.m97@gmail.com>
2021-09-20 00:01:40 +0300

    vbc

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <nav className="navbar"></nav>
+      <main className="main"></main>
+      <footer className="footer"></footer>
+
+    </div>
+  );
+}
+
+export default App;
