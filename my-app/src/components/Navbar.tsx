11dd71178022a7cda68c62047ab4d1a0fcd60197
Oleh <oleh.m97@gmail.com>
2021-09-20 00:12:42 +0300

    gf

--- my-app/src/components/Navbar.tsx
+++ my-app/src/components/Navbar.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
     return (
-        <div>
-            
+        <div className="nav-container">
+            <div className="logo-container">
+                <Avatar />
+                <Typography.Title level={2} className="logo">
+                    <Link to="/">Cryptoverse</Link>
+                </Typography.Title>
+            </div>
         </div>
     );
 };
