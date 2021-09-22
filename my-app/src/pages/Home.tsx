7ba8ba17acacac2cc54c98642ee092269d19bfd8
Oleh <oleh.m97@gmail.com>
2021-09-22 23:35:25 +0300

    cryptoapi

--- my-app/src/pages/Home.tsx
+++ my-app/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React, {FC} from 'react';
-import {Col, Row, Typography} from 'antd';
+import {Col, Row, Statistic, Typography} from 'antd';
 
 const {Title} = Typography
 
@@ -10,9 +10,11 @@ const Home: FC<Props> = ({}) => {
         <>
             <Title level={2} className="heading">Global Crypto Stats</Title>
             <Row>
-                <Col span={12}>
-
-                </Col>
+                <Col span={12}><Statistic title="Total Cryptocurrencies" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
+                <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
+                <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
             </Row>
         </>
     );
---
+++ my-app/src/services/cryptoApi.ts
@@ -0,0 +1,20 @@
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+    'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_CRYPTO_KEY
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/exchanges'
+
+const createRequest = (url: string) => ({url, headers: cryptoApiHeaders})
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({baseUrl}),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: () => createRequest('/exchanges')
+        })
+    })
+})
